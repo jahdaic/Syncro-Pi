7d0db0954c3e87cd38f3c33992ec7579a8ce37e5
Jahdai Cintron <jahdaic@yahoo.com>
2022-01-08 18:12:16 -0500

    Add Weather Icons and Geolocation

--- src/components/widgets/Weather/Weather.jsx
+++ src/components/widgets/Weather/Weather.jsx
@@ -7,16 +7,28 @@ import '../../../css/weather.css';
 
 export const Weather = props => {
 	const [weather, setWeather] = useState(null);
+	const [location, setLocation] = useState(null);
 
-	const updateWeather = () => {
+	const updateWeather = position => {
 		if(!process.env.REACT_APP_OPEN_WEATHER_API_KEY) {
 			setWeather(false);
 			return;
 		}
 
+		if(!position && !location) {
+			setLocation(false);
+			setWeather(false);
+			return;
+		}
+
+		setLocation(position);
+
 		const weatherAPI = new OpenWeatherAPI({
 			key: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
-			locationName: "Port Orange, Florida",
+			coordinates: {
+				lat: position?.coords?.latitude || location?.coords?.latitude,
+				lon: position?.coords?.longitude || location?.coords?.longitude
+			},
 			units: "imperial"
 		})
 
@@ -29,6 +41,57 @@ export const Weather = props => {
 			});
 	};
 
+	const getLocation = () => {
+		if(!navigator.geolocation) {
+			setWeather(false);
+			setLocation(false);
+			return;
+		}
+
+		navigator.geolocation.getCurrentPosition(updateWeather, err => {
+			setWeather(false);
+			setLocation(false);
+		});
+	}
+
+	const getWeatherIcon = code => {
+		const id = 'weather-icon';
+
+		switch(code) {
+			case '01d':
+				return <Icon.Sun id={id} />;
+			case '01n':
+				return <Icon.MoonStars id={id} />;
+			case '02d':
+				return <Icon.CloudSun id={id} />;
+			case '02n':
+				return <Icon.CloudMoon id={id} />;
+			case '03d':
+			case '03n':
+				return <Icon.Cloud id={id} />;
+			case '04d':
+			case '04n':
+				return <Icon.Clouds id={id} />;
+			case '09d':
+			case '09n':
+				return <Icon.CloudDrizzle id={id} />;
+			case '10d':
+			case '10n':
+				return <Icon.CloudRainHeavy id={id} />;
+			case '11d':
+			case '11n':
+				return <Icon.CloudLightningRain id={id} />;
+			case '13d':
+			case '13n':
+				return <Icon.Snow2 id={id} />;
+			case '50d':
+			case '50n':
+				return <Icon.CloudHaze2 id={id} />;
+			default:
+				return <Icon.QuestionLg id={id} />;
+		}
+	}
+
 	const degreesToCompass = direction => {
 		const directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
 		const angle = direction % 360;
@@ -38,18 +101,29 @@ export const Weather = props => {
 	};
 
 	useEffect(() => {
-		const interval = setInterval(updateWeather, 1800000); // 30 minutes
+		const interval = setInterval(getLocation, 1800000); // 30 minutes
 
 		return () => clearInterval(interval);
 	}, []);
 
-	useEffect(updateWeather, []);
+	useEffect(getLocation, []);
+
+	console.log('Weather', weather);
 
 	if(weather === null) return (
 		<div id="weather">
 			<Icon.CloudArrowDown id="weather-icon" />
 			<div id="weather-description">
 				Weather loading...
+			</div>
+		</div>
+	);
+
+	if(location === false ) return (
+		<div id="weather">
+			<Icon.PinMap id="weather-icon" />
+			<div id="weather-description">
+				Could not get location
 			</div>
 		</div>
 	);
@@ -66,7 +140,7 @@ export const Weather = props => {
 	return (
 		<div id="weather">
 			<div id="weather-top">
-				<Icon.CloudSun id="weather-icon" />
+				{getWeatherIcon(weather.icon.raw)}
 				<div id="weather-main">
 					<div id="weather-temp">
 						{`${Number(weather?.temp.cur).toFixed(0)}°`}
@@ -80,7 +154,7 @@ export const Weather = props => {
 			<div id="weather-bottom">
 				<div id="weather-rain">
 					<Icon.DropletHalf />
-					{`${weather?.rain}%`}
+					{`${weather?.rain}"`}
 				</div>
 				<div id="weather-wind">
 					<Icon.Wind />
